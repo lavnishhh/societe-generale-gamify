7a7b54341882b7e835d503623d0bfc0f242a5003
Lavnish <lavnishdesigner@gmail.com>
2024-07-13 15:21:07 +0530

    added files

---
+++ android/app/src/main/kotlin/com/example/societe_generale_gamification/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.societe_generale_gamification
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/create_test_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CreateTestScreen extends StatefulWidget {
+  const CreateTestScreen({super.key});
+
+  @override
+  State<CreateTestScreen> createState() => _CreateTestScreenState();
+}
+
+class _CreateTestScreenState extends State<CreateTestScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
